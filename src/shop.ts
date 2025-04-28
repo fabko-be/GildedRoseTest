7ec73ea5f4d22b86f50d1df57e44da1cb2ef8388
Fa <fabianquoidbach@gmail.com>
2025-04-29 00:09:25 +0200

    Add quality Const + Legend Sellin Const

--- src/shop.ts
+++ src/shop.ts
@@ -1,5 +1,9 @@
 import Item from "./item";
 
+export const MAX_QUALITY = 50;
+export const MIN_QUALITY = 0;
+export const LEGENDARY_SELLIN = Infinity
+
 export default class Shop {
   private readonly items: Item[];
   constructor(items: Item[] = []){
